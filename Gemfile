f19278bcf2dced6a6d0afa6978c947b847aed0e7
Nim <drakontis.panagiotis@gmail.com>
2014-01-12 19:01:00 +0200

    Closes #38 Select chosen has been added to the application

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 
   gem 'bootstrap-sass'
+
+  gem 'chosen-rails'
 end
 
 gem 'jquery-rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require chosen-jquery
 //= require_tree .
+
+// enable chosen js
+$(function($){
+    $('.has-chosen').chosen();
+});
